d414fd8c3ab4a9ec72a171b318bb4701ebcdfeda
Nicolas Botello <nicolas.botello@zumepizza.com>
2019-06-28 11:41:10 -0700

    fail test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,6 @@
-describe('page', () => {
-  it('works', () => {})
-})
+import lighthouse from 'lighthouse';
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(true);
+  });
+});
